aac754236a1e19f4bd10f6a7e45a492c2fda8010
lihiitz <lihiitz@gmail.com>
2020-05-06 12:25:32 +0300

    DOM Traversal

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,15 @@
 
 const render = new Renderer()
-$(`button`).on(`click`, function(data){
+$(`button`).on(`click`, function(){
     const ingredient = $(`input`).val()
     $.get(`/recipes/${ingredient}`, function(res){
         let recipes = res
         render.render(recipes)
         console.log((recipes));
     })
+})
+$(`#container`).on(`click`, `img`, function(){
+    let ingredients = $(this).closest(`div`).find(`p`).text()
+    ingredients = ingredients.split(`:`)
+    console.log(ingredients[1].split(`,`)[0])
 })
